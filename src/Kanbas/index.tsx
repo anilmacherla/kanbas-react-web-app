0e66cea455f247b54360072379d3a5d8ddf4ed6f
anilmacherla <anilmacherla5@gmail.com>
2024-02-18 13:56:53 -0500

    Labs Completed

---
+++ src/Kanbas/index.tsx
@@ -0,0 +1,14 @@
+import Nav from "../Nav";
+
+function Kanbas() {
+    return (
+        <div>
+            <Nav />
+            <h1>Kanbas</h1>
+        </div>
+    )
+}
+
+
+export default Kanbas
+
