c2c16bb4e46f2ef3fa6408b001b30ef17dd78167
anilmacherla <anilmacherla5@gmail.com>
2024-03-16 17:41:27 -0400

    My Changes

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import ReduxExamples from "./ReduxExamples";
+import TodoList from "../a3/todos/TodoList";
+
+function sayHello() {
+    alert("Hello");
+}
 
 const Assignment4 = () => {
     return (
-        <div>Assignment4</div>
-    )
-}
+        <>
+            <h1>Assignment 4</h1>
+            <ReduxExamples />
+            <TodoList />
 
-export default Assignment4
+            <ClickEvent />
+            <PassingDataOnEvent />
+            <PassingFunctions theFunction={sayHello} />
+            <EventObject />
+            <Counter />
+            <BooleanStateVariables />
+            <StringStateVariables />
+            <DateStateVariable />
+            <ObjectStateVariable />
+            <ArrayStateVariable />
+            <ParentStateComponent />
+        </>
+    );
+};
+export default Assignment4;
+
